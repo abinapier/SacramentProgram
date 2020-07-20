47fdb907a8b3404151b99e0cb7a7faf4aad35248
abinapier <abi.napier@yahoo.com>
2020-07-20 13:22:22 -0600

    Song model added

---
+++ SacramentProgram/Models/Song.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SacramentProgram.Models
+{
+    public class Song
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int PageNum { get; set; }
+    }
+}
