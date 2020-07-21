03ceecfac442b131d58b313050291aec6d3d6400
thorne-lauren <laelthorne@gmail.com>
2020-07-21 13:18:03 -0600

    meeting start

---
+++ SacramentProgram/Pages/Speakers/MeetingPicker.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using SacramentProgram.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace SacramentProgram.Pages.Speakers
+{
+    public class MeetingPickerPageModel : PageModel
+    {
+        public SelectList MeetingDatesSL { get; set; }
+
+        public void PopulateMeetingsDropDownList(SacramentProgramContext _context,
+            object selectedMeeting = null)
+        {
+            var meetingQuery = from m in _context.Meeting
+                                   orderby m.MeetingDate 
+                                   select m.MeetingDate;
+
+            MeetingDatesSL = new SelectList(meetingQuery,
+                        "ID", "MeetingDate", selectedMeeting);
+        }
+    }
+}
