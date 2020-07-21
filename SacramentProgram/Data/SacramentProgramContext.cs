750572ffab1005db186d4a805c3e3e6a0eeb5fe5
abinapier <abi.napier@yahoo.com>
2020-07-21 09:42:57 -0600

    data

---
+++ SacramentProgram/Data/SacramentProgramContext.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SacramentProgram.Models;
+
+namespace SacramentProgram.Data
+{
+    public class SacramentProgramContext : DbContext
+    {
+        public SacramentProgramContext (DbContextOptions<SacramentProgramContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SacramentProgram.Models.Song> Song { get; set; }
+
+        public DbSet<SacramentProgram.Models.Meeting> Meeting { get; set; }
+
+        public DbSet<SacramentProgram.Models.Person> Person { get; set; }
+
+        public DbSet<SacramentProgram.Models.Speaker> Speaker { get; set; }
+
+        public DbSet<SacramentProgram.Models.MusicalNum> MusicalNum { get; set; }
+    }
+}
