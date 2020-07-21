057f5151d1e9214b5c130517c35dc9acdd09181e
wnmb99 <49930283+wnmb99@users.noreply.github.com>
2020-07-21 13:43:06 -0600

    musical nums

--- SacramentProgram/Models/MusicalNum.cs
+++ SacramentProgram/Models/MusicalNum.cs
@@ -7,5 +7,13 @@ namespace SacramentProgram.Models
         public int ID { get; set; }
         public Song Song { get; set; }
         public string Performer { get; set; }
+        public string MusicalPerformance
+        {
+            get
+            {
+                var FullPerformance = Song + " " + Performer + " ";
+                return FullPerformance;
+            }
+        }
     }
 }
