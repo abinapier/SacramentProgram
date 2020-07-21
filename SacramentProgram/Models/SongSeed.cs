621e0c433b06c43161a55070cfc1ae3b0b1e17fc
Wright-Aaron <wri14006@gmail.com>
2020-07-21 11:49:20 -0500

    SongSeed added.

---
+++ SacramentProgram/Models/SongSeed.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SacramentProgram.Data;
+using SacramentProgram.Models;
+using System;
+using System.Linq;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SongSeed
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SacramentProgramContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<SacramentProgramContext>>()))
+            {
+                if (context.Song.Any())
+                {
+                    return;
+                }
+
+                context.Song.AddRange(
+                    new Song
+                    {
+                        Name = "The Morning Breaks",
+                        PageNum = 1
+                    },
+
+                    new Song
+                    {
+                        Name = "The Spirit of God",
+                        PageNum = 2
+                    },
+
+                    new Song
+                    {
+                        Name = "Now Let Us Rejoice",
+                        PageNum = 3
+                    },
+
+                    new Song
+                    {
+                        Name = "Truth Eternal",
+                        PageNum = 4
+                    },
+
+                    new Song
+                    {
+                        Name = "Redeemer of Isreal",
+                        PageNum = 5
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
