005c45dbed6b6e1fdff088560fb7967133e3a8e3
Wright-Aaron <wri14006@gmail.com>
2020-07-21 12:14:41 -0500

    Added PersonSeed.

--- SacramentProgram/Program.cs
+++ SacramentProgram/Program.cs
@@ -30,6 +30,16 @@ namespace SacramentProgram
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occurred seeding the DB.");
                 }
+
+                try
+                {
+                    PersonSeed.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
             }
 
             host.Run();
